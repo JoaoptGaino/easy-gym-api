49301acd77ed783e1f97476fba1f87e376bc0ca9
joaoptgaino <joaoptgaino@hotmail.com>
2020-09-29 23:32:30 -0300

    Feel changes on routes and added another controller called days

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 const ExerciseController = require('./controllers/exercisesController.json');
+const DaysController = require('./controllers/daysController.json');
 routes.get('/',(req,res)=>{
     res.send("Main page");
 })
-routes.get("/api",(req,res)=>{
+routes.get("/api/exercises",(req,res)=>{
     res.send(ExerciseController);
+})
+routes.get("/api/days",(req,res)=>{
+    res.send(DaysController);
 })
 
 
